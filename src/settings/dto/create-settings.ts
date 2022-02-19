1ead6081c1eaf8de36c2fd1416be2d5d1225d2c6
Byecom <sunil@byecom.in>
2022-02-19 15:45:37 +0530

    this is just testing

--- src/settings/dto/create-settings.ts
+++ src/settings/dto/create-settings.ts
@@ -5,11 +5,17 @@ import {
   IsBoolean,
   IsArray,
 } from 'class-validator';
-import { ApiProperty, ApiParam } from '@nestjs/swagger';
+import { ApiProperty, ApiParam, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateSettingsDto {
   @IsArray()
   @IsNotEmpty()
   @ApiProperty()
   delivery_service_array: Array<object>;
+
+  @IsString()
+  @ApiPropertyOptional({
+    description:'this is just testing',
+  })
+  meta_value:string;
 }
