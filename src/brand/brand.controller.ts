f946f622523f62520e3a84ed3642d78e3082e1ca
sorathiyavatsal <vatsalsorathiya22@gmail.com>
2022-02-23 13:24:37 +0530

    create all api's of brand

--- src/brand/brand.controller.ts
+++ src/brand/brand.controller.ts
@@ -1,14 +1,30 @@
-import { Controller, Post, UseGuards, Request, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Request,
+  Get,
+  Param,
+  Put,
+  Response,
+  Delete,
+  UseInterceptors,
+  UploadedFiles,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { FilesInterceptor } from '@nestjs/platform-express';
 import {
   ApiBearerAuth,
   ApiBody,
   ApiConsumes,
+  ApiParam,
   ApiSecurity,
   ApiTags,
 } from '@nestjs/swagger';
 import { Roles } from 'src/auth/roles.decorator';
 import { BrandService } from './brand.service';
+import { diskStorage } from 'multer';
+import { imageFileFilter } from 'src/delivery_fleet/deliveryfleet.controller';
 
 @Controller('brand')
 @ApiTags('Brand')
@@ -20,12 +36,33 @@ export class BrandController {
   @Get('/all')
   @UseGuards(AuthGuard('jwt'))
   @Roles('ADMIN')
-  async getAllBrand(@Request() request) {
-    return await this.BrandService.getAllBrand();
+  async getAllBrand(@Response() response) {
+    response.json(await this.BrandService.getAllBrand());
+  }
+
+  @Get('/:id')
+  @UseGuards(AuthGuard('jwt'))
+  @Roles('ADMIN')
+  @ApiParam({ name: 'id', required: true })
+  async getBrand(@Param() params, @Response() response) {
+    response.json(await this.BrandService.getBrand(params.id));
   }
 
   @Post('/add')
   @UseGuards(AuthGuard('jwt'))
+  @UseInterceptors(
+    FilesInterceptor('brandImage', 20, {
+      storage: diskStorage({
+        destination: './public/uploads/brands',
+        filename: function (req, file, cb) {
+          let extArray = file.mimetype.split('/');
+          let extension = extArray[extArray.length - 1];
+          cb(null, file.fieldname + '-' + Date.now() + '.' + extension);
+        },
+      }),
+      fileFilter: imageFileFilter,
+    }),
+  )
   @ApiBody({
     schema: {
       type: 'object',
@@ -47,7 +84,77 @@ export class BrandController {
     },
   })
   @ApiConsumes('multipart/form-data', 'application/json')
-  async postBrand(@Request() request) {
-    return await this.BrandService.postBrand();
+  async postBrand(@Request() request, @UploadedFiles() files) {
+    const response = [];
+    if (files && files.length > 0) {
+      files.forEach((file) => {
+        const fileReponse = file.path;
+        response.push(fileReponse);
+      });
+    }
+    request.body.brandImage = response[0];
+    return await this.BrandService.postBrand(request.body);
+  }
+
+  @Put('/update:id')
+  @UseGuards(AuthGuard('jwt'))
+  @UseInterceptors(
+    FilesInterceptor('brandImage', 20, {
+      storage: diskStorage({
+        destination: './public/uploads/brands',
+        filename: function (req, file, cb) {
+          let extArray = file.mimetype.split('/');
+          let extension = extArray[extArray.length - 1];
+          cb(null, file.fieldname + '-' + Date.now() + '.' + extension);
+        },
+      }),
+      fileFilter: imageFileFilter,
+    }),
+  )
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        brandName: {
+          type: 'string',
+        },
+        brandImage: {
+          type: 'string',
+          format: 'binary',
+        },
+        description: {
+          type: 'string',
+        },
+        status: {
+          type: 'boolean',
+        },
+      },
+    },
+  })
+  @ApiConsumes('multipart/form-data', 'application/json')
+  @ApiParam({ name: 'id', required: true })
+  async putBrand(
+    @Param() params,
+    @Request() request,
+    @Response() response,
+    @UploadedFiles() files,
+  ) {
+    const responsedata = [];
+    if (files && files.length > 0) {
+      files.forEach((file) => {
+        const fileReponse = file.path;
+        responsedata.push(fileReponse);
+      });
+    }
+    request.body.brandImage = responsedata[0];
+    response.json(await this.BrandService.putBrand(request.body, params.id));
+  }
+
+  @Delete('/delete:id')
+  @UseGuards(AuthGuard('jwt'))
+  @ApiConsumes('multipart/form-data', 'application/json')
+  @ApiParam({ name: 'id', required: true })
+  async deleteBrand(@Param() params, @Request() request, @Response() response) {
+    response.json(await this.BrandService.deleteBrand(params.id));
   }
 }
--- src/brand/brand.module.ts
+++ src/brand/brand.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BrandController } from './brand.controller';
+import { BrandsSchema } from './brand.model';
 import { BrandService } from './brand.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Brands', schema: BrandsSchema }]),
+  ],
   controllers: [BrandController],
   providers: [BrandService],
 })
